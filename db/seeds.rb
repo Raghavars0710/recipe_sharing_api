d3d832b9240ff8e9868682789326c7c4f910deed
Raghavendra Sharma <sharmaraghavendra42@gmail.com>
2023-08-09 00:16:02 +0530

    write user ,recipe and favorite data in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,51 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+# user1 = User.create!(
+#   name: "Admin User",
+#   username: "admin",
+#   role: "admin",
+#   email: "admin@gmail.com",
+#   password: "123456",
+#   password_confirmation: "123456"
+# )
+
+# user2 = User.create!(
+#   name: "Regular User",
+#   username: "user",
+#   role: "customer",
+#   email: "user@gmail.com",
+#   password: "123456",
+#   password_confirmation: "123456"
+# )
+
+# Recipes
+# recipe1 = Recipe.create!(
+#   title: "Paneer Tikka Masala",
+#   ingredients: "200g paneer, yogurt, spices...",
+#   instructions: "1. Marinate paneer in yogurt and spices. 2. Grill or cook in a pan...",
+#   tags: "paneer, tikka, indian",
+#   user_id: 
+# )
+
+# recipe2 = Recipe.create!(
+#   title: "Chana Masala",
+#   ingredients: "1 cup chickpeas, tomatoes, onions, spices...",
+#   instructions: "1. Cook chickpeas. 2. Prepare a tomato-onion gravy with spices...",
+#   tags: "chana masala, chickpeas, indian",
+#   user_id: 
+# )
+
+# Favorite.create!(
+#   user_id: 6,
+#   recipe_id: 1
+# )
+
+# Favorite.create!(
+#   user_id: 6,
+#   recipe_id: 2
+# )
+
+# puts "Seed data created successfully!"
